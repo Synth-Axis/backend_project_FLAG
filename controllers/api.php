69d79966701e3d6e35429141feb2197281c3bc63
Synth-Axis <synth-etiks@hotmail.com>
2023-10-19 21:36:38 +0100

    DB API insert

---
+++ controllers/api.php
@@ -0,0 +1,17 @@
+<?php
+
+require("../models/api.php");
+
+$model = new Api();
+
+// $model->getGames($games);
+
+// $model->getGenres($genres);
+
+// $model->getRatings($games);
+
+// $model->getScreenshots($games);
+
+// $model->getPlatforms($platforms);
+
+$model->getGamesByPlatforms($platforms);
