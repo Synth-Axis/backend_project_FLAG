0afb3d98bb48b266e91acf4f33b0b4b7bab07a7a
Synth-Axis <synth-etiks@hotmail.com>
2023-11-16 18:57:05 +0000

    added conditional validation of page for JS buttons

--- js/index.js
+++ js/index.js
@@ -3,54 +3,58 @@ document.addEventListener("DOMContentLoaded", () => {
     const deleteButtons = document.querySelectorAll("#removeGame");
     const addButton = document.querySelector("#addGame");
 
-    for (let deleteButton of deleteButtons) {
+    if (window.location.href.includes("userlibrary")) {
+        for (let deleteButton of deleteButtons) {
     
-        const game = deleteButton.nextElementSibling.value;
+            const game = deleteButton.nextElementSibling.value;
     
-        deleteButton.addEventListener("click", () => {
+            deleteButton.addEventListener("click", () => {
 
-            const gameCard = deleteButton.parentNode.parentNode.parentNode;
+                const gameCard = deleteButton.parentNode.parentNode.parentNode;
+
+                fetch("/requests/", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: "request=removeGameFromOwned&game_id=" + game
+                })
+                    .then(response => response.json())
+                    .then(result => {
+                        if (result.message === "OK") {
+                            gameCard.remove();
+                            let gamesCounter = parseInt(document.querySelector("#gameCounter").textContent);
+                            gamesCounter = gamesCounter - 1;
+                            document.querySelector("#gameCounter").textContent = gamesCounter;
+                            console.log(gamesCounter);
+                        }
+                    });
+            });
+        }
+    }
+
+    if (window.location.href.includes("gamedetail")) {
+        addButton.addEventListener("click", () => {
+
+            const gameId = addButton.dataset.game_id;
 
             fetch("/requests/", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/x-www-form-urlencoded"
                 },
-                body: "request=removeGameFromOwned&game_id=" + game
+                body: "request=addGamesToUserList&game_id=" + gameId
             })
-            .then(response => response.json())
-            .then(result => {
-                if (result.message === "OK") {
-                    gameCard.remove();
+                .then(response => response.json())
+                .then(result => {
+                    const p = document.createElement("p");
+                    document.querySelector("#innerContainer").appendChild(p).textContent = result.message;
                     let gamesCounter = parseInt(document.querySelector("#gameCounter").textContent);
-                    gamesCounter = gamesCounter - 1;
+                    gamesCounter = gamesCounter + 1;
                     document.querySelector("#gameCounter").textContent = gamesCounter;
-                    console.log(gamesCounter);
-                }
-            });
+                });
         });
     }
-
-    addButton.addEventListener("click", () => {
-
-        const gameId = addButton.dataset.game_id;
-
-        fetch("/requests/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            body: "request=addGamesToUserList&game_id=" + gameId
-        })
-            .then(response => response.json())
-            .then(result => {
-                const p = document.createElement("p");
-                document.querySelector("#innerContainer").appendChild(p).textContent = result.message;
-                let gamesCounter = parseInt(document.querySelector("#gameCounter").textContent);
-                gamesCounter = gamesCounter + 1;
-                document.querySelector("#gameCounter").textContent = gamesCounter;
-            });
-    });
 });
 
     
